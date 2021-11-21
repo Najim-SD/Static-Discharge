0b9873b1c4984e51e8dcd350a17fea01dd2762f6
Awab M.Najim <44520076+Gandol-SD@users.noreply.github.com>
2021-11-21 20:46:24 +0200

    Add Level Intro and Outro Animations

--- Levels/Level.gd
+++ Levels/Level.gd
@@ -12,6 +12,8 @@ func _ready():
 	UI_Ref = $"Gameplay HUD"
 	Bot_Ref = $YSort/Objects/Bot
 	UI_Ref.find_node("Title").text = "#" + levelNumber + " - " + levelName
+	$"Gameplay HUD/UI_Player".stop(true)
+	$"Gameplay HUD/UI_Player".play("LevelIntro",-1, 0.8)
 	pass # Replace with function body.
 
 func set_CL_Label(level:int):
@@ -28,5 +30,17 @@ func _on_Bot_step():
 
 
 func _on_Bot_reachedGoal():
-	get_tree().reload_current_scene() # for now
+	$"Gameplay HUD/UI_Player".stop(true)
+	$"Gameplay HUD/UI_Player".play("LevelOutro")
+	pass # Replace with function body.
+
+
+func _on_GlitchTimer_timeout():
+	$"Gameplay HUD/Glitch".material.set_sh
+	pass # Replace with function body.
+
+
+func _on_UI_Player_animation_finished(anim_name):
+	if anim_name == "LevelOutro":
+		get_tree().reload_current_scene()
 	pass # Replace with function body.
